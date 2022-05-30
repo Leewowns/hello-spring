dbc5bc54963aa659f5f79b8b914cab39d95da5a5
LEEWOWNS <leewowns91@gmail.com>
2022-05-30 21:27:09 +0900

    [2022-05-28 이재준] 자바 코드로 스프링 빈 등록(SpringConfig)

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -9,6 +9,24 @@ public class MemberController {
 
     private final MemberService memberService;
 
+    /*
+    * DI : setter 주입 // 누구나 어디서  memberService.serMemberRepository() 로 갈아끼울 수 있다.
+    * */
+//    @Autowired
+//    public void setMemberService(MemberService memberService) {
+//        this.memberService = memberService;
+//    }
+
+
+    /*
+   * DI : 필드 주입 // 바꿀 수 있는 방법이 없다.
+  * */
+//    @Autowired private MemberService memberService;
+
+
+    /*
+    *  DI : 생성자 주입 // 권장하는 주입
+    * */
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
